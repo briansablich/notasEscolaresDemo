5fef2bb70640ecc5b011b8bb46619f3b4e74607d
Brian_Sablich <sablichbrian@yahoo.com.ar>
2026-02-06 20:13:24 -0300

    first commit

---
+++ src/main/java/com/demo/notasEscolaresDemo/NotasEscolaresDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.notasEscolaresDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotasEscolaresDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotasEscolaresDemoApplication.class, args);
+	}
+
+}
